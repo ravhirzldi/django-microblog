bdadf79dc4b4c725611dd47ca16e870deb0ff648
Ravhi Rizaldi <ravhi.rizaldi@outlook.com>
2017-10-28 23:08:25 +0700

    Fixed disquss and Debug set to True

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#bwfo2j^)bi+thp5k!g#@-tvqsu5xpk-m^wunvfd7p+4a5syos'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'ravhirizaldi.pythonanywhere.com']
 

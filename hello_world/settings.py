a2ce142723bb69b58f42613d09ef10526ab164fe
Ravhi Rizaldi <ravhi.rizaldi@outlook.com>
2017-10-28 21:52:37 +0700

    Fixed ALLOWED_HOST and Debug

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#bwfo2j^)bi+thp5k!g#@-tvqsu5xpk-m^wunvfd7p+4a5syos'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','ravhirizaldi.pythonanywhere.com ']
 
 
 # Application definition

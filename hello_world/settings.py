1b192f87d139ac1d93d83a0c3c9febf864525f75
Ravhi Rizaldi <ravhi.rizaldi@outlook.com>
2017-10-31 01:20:59 +0700

    fixed image url

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "/static/media/"
 MEDIA_ROOT = os.path.join(PROJECT_DIR, "media")
 
 CKEDITOR_UPLOAD_SLUGIFY_FILENAME = False

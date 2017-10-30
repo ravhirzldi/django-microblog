e2d12988889009c2d17b1ff6b1602cb27a2c121d
Ravhi Rizaldi <ravhi.rizaldi@outlook.com>
2017-10-31 01:31:48 +0700

    fixed image url

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from microblog import views
 
+# URL Pattern is here
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # include URL pattern from microblog/urls.py

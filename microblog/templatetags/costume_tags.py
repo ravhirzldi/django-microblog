b021305133587a624fefd7ca28566be5b82e4c0f
Ravhi Rizaldi <ravhi.rizaldi@outlook.com>
2017-11-02 13:06:19 +0700

    change navbar layout

--- microblog/templatetags/costume_tags.py
+++ microblog/templatetags/costume_tags.py
@@ -1,8 +1,13 @@
 from django import template
 from ..models import Post
+from django.contrib.auth.models import User
 register = template.Library()
 
 # Get total posts counts for rendering in template
 @register.simple_tag
 def total_post():
     return Post.published.count()
+
+@register.simple_tag
+def total_user():
+    return User.objects.count()
